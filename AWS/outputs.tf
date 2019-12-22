7ad17888b18f6dfcae4b82a97312254559676cab
paak <par.akerstrom@nasdaq.com>
2019-12-22 13:40:22 +0100

    root and network module

--- AWS/outputs.tf
+++ AWS/outputs.tf
@@ -8,3 +8,16 @@ output "Bucket Name" {
 output "Bucket ARN" {
   value = "${module.storage.bucket_arn}"
 }
+
+#----networking outputs----
+output "public_subnets" {
+  value = "${module.networking.public_subnets}"
+}
+
+output "public_sg" {
+  value = "${module.networking.public_sg}"
+}
+
+output "subnet_ips" {
+  value = "${module.networking.subnet_ips}"
+}
